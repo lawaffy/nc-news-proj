e619ced119f96b924193fe7bf631c7ff949b727c
lawaffy <laurawathen@hotmail.com>
2025-01-27 12:27:21 +0000

    GET /api endpoint available

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 # Northcoders News API
 
-For instructions, please head over to [L2C NC News](https://l2c.northcoders.com/courses/be/nc-news).
+You will need to create two dotenv files to connect to the databases locally. Check the setup.sql file for the database name!
 
-
-
---- 
-
-This portfolio project was created as part of a Digital Skills Bootcamp in Software Engineering provided by [Northcoders](https://northcoders.com/)
+1) .env.test which contains: PGDATABASE=database_name_test
+2) .env.development which contains: PGDATABASE=database_name
--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,21 @@
 const endpointsJson = require("../endpoints.json");
+const request = require("supertest");
+const app = require('../app')
+const seed = require('../db/seeds/seed')
+const db = require('../db/connection')
+const testData = require('../db/data/test-data/index')
+
 /* Set up your test imports here */
 
 /* Set up your beforeEach & afterAll functions here */
+
+beforeEach(() => {
+  return seed(testData)
+})
+
+afterAll(() => {
+  return db.end();
+})
 
 describe("GET /api", () => {
   test("200: Responds with an object detailing the documentation for each endpoint", () => {
